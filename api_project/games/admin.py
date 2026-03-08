8f37ca2199c30a49c210b520d732e87058f67928
yinhoe <yinthehoe@gmail.com>
2026-03-08 23:49:53 +0000

    Updated the test outlines in tests/test_api.py to match the new API endpoints from the project redesign.

--- api_project/games/admin.py
+++ api_project/games/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, Nutrition, UserIngredients
+from django.contrib.auth.admin import UserAdmin
+from .models import Game, Genre, Review, Platform, Developer, Publisher, User
+
 
 # Register your models here.
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
-admin.site.register(Nutrition)
-admin.site.register(UserIngredients)
+admin.site.register(Game)
+admin.site.register(Genre)
+admin.site.register(Review)
+admin.site.register(Platform)
+admin.site.register(Developer)
+admin.site.register(Publisher)
+admin.site.register(User, UserAdmin)
--- api_project/tests/test_api.py
+++ api_project/tests/test_api.py
@@ -4,33 +4,37 @@ Unit tests for API methods in games.views.
 import pytest
 from games import views
 
+VALID_USER_ID = 1
+INVALID_USER_ID = 0
+VALID_GAME_ID = 1
+INVALID_GAME_ID = 0
 
 class TestUsers:
     """
-    Tests for API endpoints on users: POST users, GET users/{user_id},
-    PUT users/{user_id}, DELETE users/{user_id}, and GET users/{user_id}/recipes.
-    
-    Also tests authentication using Django's auth.User model.
+    Tests signup and login via Django's provided User model, as well as the
+    GET /users/{user_id} API endpoint.
     """
         
-    @pytest.mark.parametrize("password", ["validpassword", "tooshort", "546123457853245"])
+    @pytest.mark.parametrize("password", ["validpassword",
+                                          "tooshort",
+                                          "546123457853245"])
     def test_post_passwords(self, password):
         """
-        Tests POST users by signing up with a valid username and a variety of passwords,
-        in order of:
-        a valid password, a password that is too short, password that is numeric.
+        Tests user sign up using:
+        valid password, password that is too short, password that is numeric.
         
-        Passes when first password is successful and returns a 201 HTTP Created,
-        and the rest return HTTP 400 Bad Request errors.
+        Passes when:
+        - Valid password succeeds and returns a 201 HTTP Created.
+        - Invalid passwords return a HTTP 400 Bad Request.
         """
         # Remember to check that the user does not exist beforehand (for first test)
         assert False
     
     def test_post_username_taken(self):
         """
-        Tests POST users by signing up with a username that is not unique.
+        Tests user sign up using a username that is not unique.
         
-        Passes when it returns a HTTP 409 Conflict error.
+        Passes when it returns a HTTP 409 Conflict.
         """
         assert False
     
@@ -43,187 +47,246 @@ class TestUsers:
         # Login with valid credentials using fixture, check that it was successful (auth.get_user)
         assert False
     
-    @pytest.mark.parametrize("user_id", [1, 0])
-    def test_get_id_valid(self, user_id):
+    @pytest.mark.parametrize("user_id", [VALID_USER_ID, INVALID_USER_ID])
+    def test_get(self, user_id):
         """
-        Tests GET users/{user_id} on a valid and invalid user_id
+        Tests GET /users/{user_id} on a valid and invalid user_id
         
-        Passes when the first returns a correct user information and a HTTP 200 OK,
-        and the second returns a HTTP 404 Not Found error.
-        """
-        assert False
-    
-    @pytest.mark.parametrize("user_id", [1, 2, 0, -1])
-    def test_put_id(self, user_id):
-        """
-        Tests PUT users/{user_id} for an authenticated user on their own user_id,
-        someone else's user_id, for an unauthenticated user, and an invalid id.
-        
-        Passes when the first commits changes successfully with a HTTP 200 OK,
-        the second returns a HTTP 403 Forbidden error, the third returns a 401
-        Unauthorized error, and the fourth returns a HTTP 404 Not Found error.
-        """
-        assert False
-        
-    @pytest.mark.parametrize("user_id", [2, 3, 0, -1])
-    def test_delete_id(self, user_id):
-        """
-        Tests DELETE users/{user_id} for an authenticated user on their own user_id
-        and someone else's user_id, for an unauthenticated user, and for an invalid
-        user_id.
-        
-        Passes when the first successfully deletes a user and returns a HTTP
-        200 OK, the second returns a HTTP 403 Forbidden error, the third
-        returns a HTTP 401 Unauthorized error, and the fourth returns a HTTP 404
-        Not Found error.
-        """
-        assert False
-    
-    @pytest.mark.parametrize("user_id", [2, 3, 0, -1])
-    def test_get_id_recipes(self, user_id):
-        """
-        Tests GET users/{user_id}/recipes for an authenticated user on their own user_id
-        and someone else's user_id, for an unauthenticated user, and for an invalid
-        user_id.
-        
-        Passes when the first successfully deletes a user and returns a HTTP
-        200 OK, the second returns a HTTP 403 Forbidden error, the third
-        returns a HTTP 401 Unauthorized error, and the fourth returns a HTTP 404
-        Not Found error.
+        Passes when:
+        - Valid user_id returns correct user information and a HTTP 200 OK.
+        - Invalid user_id returns a HTTP 404 Not Found.
         """
         assert False
 
 
-@pytest.mark.parametrize("ingredient_id", [1, -1])
-def test_get_id(ingredient_id):
+class TestReviews:
     """
-    Tests GET ingredients/{ingredient_id} with a valid and invalid
-    ingredient_id.
+    Tests API endpoints for reviews:
+    - GET /games/{game_id}/reviews
+    - GET /games/{game_id}/reviews/{user_id}
+    - POST /games/{game_id}/reviews
+    - PUT /games/{game_id}/reviews/{user_id}
+    - DELETE /games/{game_id}/reviews/{user_id}
+    """
     
-    Passes when the first returns correct data and a HTTP 200 OK, and the
-    second returns a HTTP 404 Not Found error.
+    @pytest.mark.parametrize("game_id", [VALID_GAME_ID, INVALID_GAME_ID])
+    def test_get(self, game_id):
+        """
+        Tests GET /games/{game_id}/reviews for a valid and invalid game_id.
+        
+        Passes when:
+        - Valid game_id returns correct reviews and a HTTP 200 OK.
+        - Invalid game_id returns a HTTP 404 Not Found.
+        """
+        assert False
+
+    @pytest.mark.parametrize("game_id,user_id", [(VALID_GAME_ID, VALID_USER_ID),
+                                                 (VALID_GAME_ID, INVALID_USER_ID),
+                                                 (INVALID_GAME_ID, VALID_USER_ID)])
+    def test_get_id(self, game_id, user_id):
+        """
+        Tests GET /games/{game_id}/reviews/{user_id} for a variety of
+        valid and invalid game_ids and user_ids.
+        
+        Passes when:
+        - Valid IDs return the correct reviews and a HTTP 200 OK.
+        - Invalid IDs return a HTTP 404 Not Found.
+        """
+        assert False
+    
+    def test_get_own_id(self):
+        """
+        Tests GET /games/{game_id}/reviews/{user_id} for a user on their own
+        review.
+
+        Passes when it also provides a link to edit the review and returns a
+        HTTP 200 OK.
+        """
+        assert False
+
+    @pytest.mark.parametrize("game_id,authenticated", [(VALID_GAME_ID, True),
+                                                       (VALID_GAME_ID, False),
+                                                       (INVALID_GAME_ID, True)])
+    def test_post(self, game_id, authenticated):
+       """
+       Tests POST /games/{game_id}/reviews for valid and invalid game_ids on
+       authenticated and unauthenticated users.
+
+       Passes when:
+       - Valid game_id and authenticated user creates a new review and returns a
+         201 HTTP Created.
+       - Unauthenticated user returns a HTTP 401 Unauthorized.
+       - Invalid game_id returns a HTTP 404 Not Found.
+       """
+       assert False
+
+    def test_post_existing(self):
+        """
+        Tests POST /games/{game_id}/reviews when the user has an existing
+        review for that game.
+
+        Passes when it returns a HTTP 409 Conflict.
+        """
+        assert False
+
+    @pytest.mark.parametrize("game_id,authenticated", [(VALID_GAME_ID, True),
+                                                       (VALID_GAME_ID, False),
+                                                       (INVALID_GAME_ID, True)])
+    def test_put(self, game_id, authenticated):
+        """
+        Tests PUT /games/{game_id}/reviews/{user_id} for valid and invalid
+        game_ids on authenticated and unauthenticated users.
+
+        Passes when:
+        - Valid game_id and authenticated user updates the correct review and
+        returns a HTTP 200 OK.
+        - Unauthenticated user returns a HTTP 401 Unauthorized.
+        - Invalid game_id returns a HTTP 404 Not Found.
+        """
+        assert False
+
+    def test_put_forbidden(self):
+        """
+        Tests POST /games/{game_id}/reviews/{user_id} for a user trying to
+        update another user's review.
+
+        Passes when it returns a HTTP 403 Forbidden.
+        """
+        assert False
+
+    def test_put_nonexisting(self):
+        """
+        Tests POST /games/{game_id}/reviews/{user_id} for a user's nonexistent
+        review.
+
+        Passes when it returns a HTTP 404 Not Found.
+        """
+        assert False
+
+    @pytest.mark.parametrize("existent,own", [(True, True), (False, True),
+                                              (True, False)])
+    def test_delete(self, existent, own):
+        """
+        TESTS DELETE /games/{game_id}/reviews/{user_id} for an existent review,
+        non-existent review, and another user's review.
+
+        Passes when:
+        - Existent review is deleted and returns a HTTP 200 OK.
+        - Nonexistent review returns a HTTP 404 Not Found.
+        - Another user's review returns a HTTP 403 Forbidden.
+        """
+        assert False
+
+    def test_delete_unauthenticated(self):
+        """
+        TESTS DELETE /games/{game_id}/reviews/{user_id} for an unauthenticated
+        user.
+        
+        Passes when it returns a HTTP 401 Unauthorized.
+        """
+        assert False
+
+
+class TestGamesId:
+    """
+    Tests API endpoints for games/{game_id} (excluding game reviews):
+    - GET /games
+    - GET /games/{game_id}
+    - GET /games/{game_id}/analytics
+    """
+
+    def test_get(self):
+        """
+        Tests GET /games.
+
+        Passes when it returns correct data and a HTTP 200 OK.
+        """
+        assert False
+
+    @pytest.mark.parametrize("game_id", [VALID_GAME_ID, INVALID_GAME_ID])
+    def test_get_id(self, game_id):
+        """
+        Tests GET /games/{game_id} for valid and invalid game_id.
+
+        Passes when:
+        - Valid game_id returns correct data and a HTTP 200 OK.
+        - Invalid game_id returns an HTTP 404 Not Found.
+        """
+        assert False
+
+    @pytest.mark.parametrize("game_id", [VALID_GAME_ID, INVALID_GAME_ID])
+    def test_get_id_analytics(self, game_id):
+        """
+        Tests GET /games/{game_id}/amalytics for valid and invalid game_id.
+
+        Passes when:
+        - Valid game_id returns correct data and a HTTP 200 OK.
+        - Invalid game_id returns an HTTP 404 Not Found.
+        """
+        assert False
+
+
+class TestGamesCategories:
+    """
+    Tests API endpoints for different game categories:
+    - GET /games/genres
+    - GET /games/genres/{genre_id}
+    - GET /games/platforms
+    - GET /games/platforms/{platform_id}
+    - GET /games/developers
+    - GET /games/developers/{developer_id}
+    - GET /games/publishers
+    - GET /games/publishers/{publisher_id}
+    """
+
+    @pytest.mark.parametrize("category", ["genres", "platforms", "developers",
+                                          "publishers", "idonotexist"])
+    def test_category(self, category):
+        """
+        Tests the 4 API endpoints with the structure GET /games/{category}.
+
+        Passes when:
+        - They each return the correct data and a HTTP 200 OK.
+        - Non-existent category returns a HTTP 404 Not Found.
+        """
+        assert False
+
+    @pytest.mark.parametrize("category", ["genres", "platforms", "developers",
+                                          "publishers"])
+    def test_category_id_valid(self, category):
+        """
+        Tests the 4 API endpoints with the structure
+        GET /games/{category}/{category_id}, with valid category_ids.
+
+        Passes when they each return the correct data and a HTTP 200 OK.
+        """
+        assert False
+
+    @pytest.mark.parametrize("category", ["genres", "platforms", "developers",
+                                          "publishers"])
+    def test_category_id_invalid(self, category):
+        """
+        Tests the 4 API endpoints with the structure
+        GET /games/{category}/{category_id}, with invalid category_ids.
+
+        Passes when they each return a HTTP 404 Not Found.
+        """
+        assert False
+
+
+def test_method_not_allowed():
+    """
+    Tests POST /games.
+
+    Passes when it returns a HTTP 405 Method Not Allowed.
     """
     assert False
 
 
-class TestRecipes:
+def test_not_implemented():
     """
-    Tests for API endpoints on recipes: GET recipes, GET recipes?{parameter}={value},
-    GET recipes/{recipe_id}.
-    """
-    
-    def test_get_unauthenticated(self):
-        """
-        Tests GET recipes for an unauthenticated user.
-        
-        Passes when it returns correct data and a HTTP 200 OK.
-        """
-        assert False
-    
-    def test_get_authenticated(self):
-        """
-        Tests GET recipes for an authenticated user.
-        
-        Passes when it returns correct data (personalised recommendations)
-        and a HTTP 200 OK.
-        """
-        assert False
-    
-    @pytest.mark.parametrize("parameter, value", [("category", "Dessert"),
-                                                  ("category", "NotACategory"),
-                                                  ("author", "Dancer"),
-                                                  ("author", "NotAnAuthor")])
-    def test_get_filtered(self, parameter, value):
-        """
-        Tests GET recipes?{parameter}={value} for recipes filtered by a specific
-        category or author.
-        
-        There is one valid test for each parameter that should return correct
-        data and a HTTP 200 OK, and the other two tests return a HTTP 404 Not
-        Found error.
-        """
-        assert False
-    
-    @pytest.mark.parametrize("recipe_id", [1, -1])
-    def test_get_id(self, recipe_id):
-        """
-        Tests GET recipe/{recipe_id} with a valid and invalid recipe_id.
-        
-        Passes when the first returns correct data and a HTTP 200 OK, and the
-        second returns a HTTP 404 Not Found error.
-        """
-        assert False
-    
-    def test_get_id_authenticated(self):
-        """
-        Tests GET recipe/{recipe_id} for an authenticated user.
-        
-        Passes if there is an appropriate link to save the recipe for the user.
-        """
-        assert False
-    
+    Tests HEAD /games
 
-class TestUserIngredients:
+    Passes when it returns a HTTP 501 Not Implemented.
     """
-    Tests for API endpoints on a user's fridge and grocery list:
-    GET users/{user_id}/fridge, PUT users/{user_id}/fridge/{ingredient_id},
-    DELETE users/{user_id}/fridge/{ingredient_id}, GET users/{user_id}/grocery,
-    PUT users/{user_id}/grocery/{ingredient_id},
-    DELETE users/{user_id}/grocery/{ingredient_id}.
-    """
-    test_parameters = [("fridge", 1), ("fridge", 2), ("fridge", 0), ("fridge", -1),
-                       ("grocery", 1), ("grocery", 2), ("grocery", 0),
-                       ("grocery", -1)]
-    
-    @pytest.mark.parametrize("function, user_id", test_parameters)
-    def test_get_users_id(self, function, user_id):
-        """
-        Tests GET users/{user_id}/fridge and GET users/{user_id}/grocery
-        for a user on their own fridge/grocery and on another user's fridge/grocery,
-        an invalid user_id, and an unauthenticated user.
-        
-        Passes when the first scenarios return correct data and a HTTP
-        200 OK, the second returns a HTTP 403 Forbidden error, the third a HTTP
-        404 Not Found error, and the fourth returns a HTTP 401 Unauthorized error.
-        """
-        assert False
-        
-    @pytest.mark.parametrize("function, user_id", test_parameters)
-    def test_put_users_id(self, function, user_id):
-        """
-        Tests PUT users/{user_id}/fridge/{ingredient_id} and
-        PUT users/{user_id}/grocery/{ingredient_id} for a user by adding an
-        ingredient to their own fridge/grocery and to another user's fridge/grocery,
-        and for an unauthenticated user.
-        
-        Passes when the first scenarios return correct data and a HTTP
-        201 Created, the second returns a HTTP 403 Forbidden error, the third a
-        HTTP 404 Not Found error, and the fourth returns a HTTP 401 Unauthorized
-        error.
-        """
-        assert False
-    
-    
-    def test_put_users_id_reduce(self):
-        """
-        Tests PUT users/{user_id}/fridge and PUT users/{user_id}/grocery for
-        reducing a quantity of an ingredient in a fridge/grocery.
-        
-        Passes when changes are committed successfully and it returns a HTTP
-        200 OK.
-        """
-        assert False
-
-    @pytest.mark.parametrize("function, user_id", test_parameters)
-    def test_delete_users_id(self, function, user_id):
-        """
-        Tests DELETE users/{user_id}/fridge/{ingredient_id} and
-        DELETE users/{user_id}/grocery/{ingredient_id} for a user on their own
-        fridge/grocery and another user's fridge/grocery, and for an
-        unauthenticated user.
-        
-        Passes when the first scenarios delete successfully and return a HTTP
-        200 OK, the second returns a HTTP 403 Forbidden error, the third a HTTP
-        404 Not Found error, and the fourth returns a HTTP 401 Unauthorized error.
-        """
-        assert False
+    assert False
